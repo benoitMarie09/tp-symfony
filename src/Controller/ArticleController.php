825c3ad003c90bdeb405e0485c8210dc07d1f4c9
benoitMarie09 <bouduben@gmail>
2023-05-12 08:36:34 +0200

    feat : pagination de la page article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,7 +17,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController
 {
     #[Route('/create_article', name: 'app_article')]
-    public function index( Request $request, ManagerRegistry $doctrine ): Response
+    public function create( Request $request, ManagerRegistry $doctrine ): Response
+    {
+        $article = new Article(); // Création d'un article vide
+        // Création d'un objet formulaire spécifique à un article
+        $form = $this->createForm(ArticleType::class, $article);
+        // Récupération du $_GET ou du $_POST
+        $form->handleRequest($request);
+        // Récupération de l'utilisateur connecté
+        $user = $this->getUser();
+        // Si le formulaire est envoyé
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            // Ajout de la date de l'article
+            $article->setDate(new DateTime());
+            // Ajout de l'identifiant de l'utilisateur
+            $article->setUser($user);
+            // Sauvegarde dans la base de données
+            $em = $doctrine->getManager();
+            $em->persist($article);
+            $em->flush();
+            $this->addFlash('success', 'Votre article est enregistré');
+            // Redirection vers la page login
+            return $this->redirectToRoute('show_all_articles');
+        }
+        return $this->render('article/index.html.twig', [
+        'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/update_article/{id}', name: 'app_article')]
+    public function update( Request $request, ManagerRegistry $doctrine ): Response
     {
         $article = new Article(); // Création d'un article vide
         // Création d'un objet formulaire spécifique à un article
@@ -59,21 +90,30 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/', name: 'show_all_articles')]
-    public function showAllArticles( ManagerRegistry $doctrine ): Response
+    public function showAllArticles( ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request ): Response
     {
+        //$dql   = "SELECT a FROM AcmeMainBundle:Article a";
+        //$query = $em->createQuery($dql);
         $articles = $doctrine->getRepository(article::class)->findAll();
-        return $this->render('article/showAll.html.twig', [
-            'articles' => $articles,
-        ]);
+
+        $pagination = $paginator->paginate(
+            $articles, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            3 /*limit per page*/
+        );
+
+        return $this->render('article/showAll.html.twig', 
+            ['pagination' => $pagination]
+        );
     }
 
     #[Route('/articles/{id}', name: 'show_article')]
-    public function show( int $id ): Response
+    public function show( ManagerRegistry $doctrine, int $id ): Response
     {
         // Récupération de l'utilisateur connecté
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
-        $article = $user->getArticleById( $id );
+        $article = $doctrine->getRepository(article::class)->find( $id );
         if( ! $article )
         {
             throw $this->createNotFoundException( 'This article is not one of yours or doesn\'t exist.' );
